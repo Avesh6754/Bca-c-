f0f71a8ebf53dc8e9421ae670636909948b5d730
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-02-04 14:34:57 +0530

    Add files via upload

---
+++ inc and dec.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main()
+{
+//	Increment and Decrement Operators
+	int a,b,total;
+	
+	a=50;
+	b=100;
+	
+	total=++a;
+	cout<<"After the increment of a : "<<total<<" "<<endl;
+	total=--b;
+	cout<<"After the decrement of b : "<<total<<" "<<endl;
+	
+}
