e2518f71e9b89c0a2848b77a9af8095cb486324b
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-02-04 14:35:17 +0530

    Add files via upload

---
+++ use of cout .cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int age=23;
+	float per=83.3;
+	double Mynum=80.9;
+	char Gender='M';
+	bool result=true;
+	string name="Avesh";
+	
+	cout<<"My name : "<<name<<" "<<endl;
+	cout<<"My age : "<<age<<" "<<endl;
+	cout<<"My Gender : "<<Gender<<" "<<endl;
+	cout<<"My number : "<<Mynum<<" "<<endl;
+	cout<<"My percentage : "<<per<<" "<<endl;
+	cout<<"My result : "<<result<<" "<<endl;
+	
+}
+
