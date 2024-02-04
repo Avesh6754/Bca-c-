f20b41766800a3ef822ce2fcc528bfabca8190f5
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-02-04 14:34:19 +0530

    Add files via upload

---
+++ Arthmetic.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+//1)C++ Arithmetic Operators
+main()
+{
+	int a,b;
+	a=50;
+	b=9;
+	
+	cout<<"a + b : "<<a+b<<" "<<endl;
+	cout<<"a - b : "<<a-b<<" "<<endl;
+	cout<<"a * b : "<<a*b<<" "<<endl;
+	cout<<"a / b : "<<a/b<<" "<<endl;
+	cout<<"a % b : "<<a%b<<" "<<endl;
+}
