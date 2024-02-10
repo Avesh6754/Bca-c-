5ad3233f67a669c8c3af1d8c38b471c893dca083
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-02-10 12:44:33 +0530

    Add files via upload

---
+++ string program/1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	char str[50];
+	cout<<"Enter the string : ";
+	cin>>str;
+	cout<<"First string : "<<str<<" "<<endl;
+	cout<<"Enter the another string : ";
+	cin>>str;
+	
+	cout<<"Second string : "<<str<<" ";
+	return 0;	
+}
