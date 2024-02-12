b668d1f43d3fc77d5b6e9d5c65f026b39b698b4b
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-02-12 12:09:35 +0530

    Add files via upload

---
+++ Control statment/1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+main()
+{
+	int a;
+	cout<<"Enter the interger number : ";
+	cin>>a;
+	
+	if(a>0)
+	{
+		cout<<"You enter the positive number : "<<endl;
+	}
+	cout<<"This statment alway executive : ";
+}
