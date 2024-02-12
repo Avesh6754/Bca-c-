b4bcda50e66bcff6bf96a99d626947e69dd91033
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-02-12 12:09:57 +0530

    Add files via upload

---
+++ Control statment/2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+main()
+{
+	int a;
+	cout<<"Enter the integer number : ";
+	cin>>a;
+	
+	if(a>=0)
+	{
+		cout<<"You enter positive number  "<<endl;
+	}
+	else
+	{
+		cout<<"You enter negative number  "<<endl;
+	}
+	cout<<"This statment alway print : ";
+}
