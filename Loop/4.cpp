f592f9844453dac2e1c1502ad6c53df03836034d
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-02-20 12:43:22 +0530

    Add files via upload

---
+++ Loop/4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+//)Example 4: Range Based for Loop
+main()
+{
+	int n_array[]={1,2,3,4,5,6,7,8,9,10};
+	
+	for(int n:n_array)
+	{
+		cout<<" "<<n<<" ";
+	}
+} 
---
+++ Loop/5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+//1: Display Numbers from 1 to 5
+main()
+{
+	int n=5,i=1;
+	while(i<=n)
+	{
+		cout<<" "<<i<<" "<<endl;
+		i++;
+	}
+}
